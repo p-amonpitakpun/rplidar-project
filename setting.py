d226ea8fcf7d0efa252fceff1388595bfcd8bdcf
pawat <p.amonpitakpun@gmail.com>
2020-04-05 18:31:23 +0700

    add default setting

--- setting.py
+++ setting.py
@@ -4,4 +4,9 @@ import os
 load_dotenv(verbose=True)
 
 RPLIDAR_PORT = os.getenv("RPLIDAR_PORT")
+if RPLIDAR_PORT is None:
+    RPLIDAR_PORT = "COM3"
+
 BAUDRATE = int(os.getenv("BAUDRATE"))
+if BAUDRATE is None:
+    BAUDRATE = 115200
