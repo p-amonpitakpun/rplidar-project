c037cfa89903b3f28a804a0fcfe385c25d568b36
Pawat Amonpitakpun <p.amonpitakpun@gmail.com>
2020-04-28 14:48:04 +0700

    add grid_map functions

--- module/gridmap.py
+++ module/gridmap.py
@@ -4,26 +4,47 @@ import cv2 as cv
 import json
 
 
-def run_grid(windowName, dat, config):
-
-    gx, gy = config['grid_size']
-    xyrange = config['point_range']
-
-    cv.namedWindow(windowName)
-
-    while True:
-        cPoints = dat.get('p')
-
-        if cv.waitKey(10) > -1 or config.get('err') == 1:
-            break
-    cv.destroyAllWindows()
+def create_grid_map(width, height):
+    return np.zeros((height, width, 3), dtype='float32')
 
 
-if __name__ == '__main__':
-    cPoints = None
-    with open('./out/scan.json', 'r') as scanfile:
-        cPoints = json.load(scanfile)
-    if cPoints is not None:
-        pass
+def get_path_from_origin(point, g_x, g_y, x, y, x0, y0):
+    slope = point[1] / point[0]
+    path = []
+    sx = int(np.sign(x))
+    if x != 0 and x != -1:
+        for xi in range(min(x, 0), max(x, 0) + 1):
+            y1, y2 = slope * xi * g_x, slope * (xi + sx) * g_x
+            yi1 = int(np.floor(y1 / g_y))
+            yi2 = int(np.floor(y2 / g_y))
+            for yi in range(min(yi1, yi2), max(yi1, yi2) + 1):
+                if xi != x and yi != y:
+                    path.append([xi + x0, yi + y0])
     else:
-        print('error: no scanfile found')
+        for yi in range(min(y, 0), max(y, 0) + 1):
+            if yi != y:
+                path.append([x + x0, y + y0])
+    return path
+
+
+def grid_mapping(grid_map, points, g_x, g_y):
+    h, w, _ = grid_map.shape
+    x0, y0 = w // 2, h // 2
+    for point in points:
+        x, y = int(np.floor(point[0] / g_x) +
+                   x0), int(np.floor(point[1] / g_y) + y0)
+        path = get_path_from_origin(point, g_x, g_y, x - x0, y - y0, x0, y0)
+        if x < w and y < h:
+            grid_map[h - y - 1, x, 1] += 0.9
+        for x, y in path:
+            if 0 <= x < w and 0 <= y < h:
+                grid_map[h - y - 1, x, 2] += 0.7
+    return grid_map
+
+
+def create_occupancy_grid_map(points, max_width, max_height, grid_sizes):
+    grid_x, grid_y = grid_sizes
+    grid_map = create_grid_map(max_width // grid_x + 1,
+                               max_height // grid_y + 1)
+    grid_map = grid_mapping(grid_map, points, grid_x, grid_y)
+    return grid_map
