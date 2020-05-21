313212e9281eaaa9684ef0da2c5b32f2c520a8c1
Pawat Amonpitakpun <p.amonpitakpun@gmail.com>
2020-05-22 00:07:17 +0700

    edit grid_map

--- module/gridmap.py
+++ module/gridmap.py
@@ -35,10 +35,10 @@ def grid_mapping(grid_map, points, g_x, g_y):
                    x0), int(np.floor(point[1] / g_y) + y0)
         path = get_path_from_origin(point, g_x, g_y, x - x0, y - y0, x0, y0)
         if x < w and y < h:
-            grid_map[h - y - 1, x, 1] += 0.9
+            grid_map[h - y - 1, x, 1] += 1
         for x, y in path:
             if 0 <= x < w and 0 <= y < h:
-                grid_map[h - y - 1, x, 2] += 0.7
+                grid_map[h - y - 1, x, 2] += 1
     return grid_map
 
 
