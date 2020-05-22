99da9f6b663acaccbac1162e90a756e02b8303ee
Pawat Amonpitakpun <p.amonpitakpun@gmail.com>
2020-05-22 23:39:32 +0700

    add disjoint set

--- src/module/disjoint_set.py
+++ src/module/disjoint_set.py
@@ -19,14 +19,14 @@ class DisjointSet:
                 return self._disjoint_set[self._disjoint_set.index(item)]
         return None
 
-    def union(self,elem1, elem2):
+    def union(self, elem1, elem2):
         index_elem1 = self._find_index(elem1)
         index_elem2 = self._find_index(elem2)
         if index_elem1 != index_elem2 and index_elem1 is not None and index_elem2 is not None:
-            self._disjoint_set[index_elem2] = self._disjoint_set[index_elem2]+self._disjoint_set[index_elem1]
+            self._disjoint_set[index_elem2] = self._disjoint_set[
+                index_elem2] + self._disjoint_set[index_elem1]
             del self._disjoint_set[index_elem1]
         return self._disjoint_set
-        
+
     def get(self):
         return self._disjoint_set
-	
